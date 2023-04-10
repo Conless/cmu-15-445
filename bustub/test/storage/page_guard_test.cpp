cc69a8ca44f0ecf3b67776504af49b5f030e9976
Conless <conlesspan@outlook.com>
2023-04-10 20:31:25 +0800

    feat(page_guard): finish page_guard and pass the test.

--- bustub/test/storage/page_guard_test.cpp
+++ bustub/test/storage/page_guard_test.cpp
@@ -23,7 +23,7 @@
 namespace bustub {
 
 // NOLINTNEXTLINE
-TEST(PageGuardTest, DISABLED_SampleTest) {
+TEST(PageGuardTest, SampleTest) {
   const std::string db_name = "test.db";
   const size_t buffer_pool_size = 5;
   const size_t k = 2;
@@ -43,6 +43,45 @@ TEST(PageGuardTest, DISABLED_SampleTest) {
   guarded_page.Drop();
 
   EXPECT_EQ(0, page0->GetPinCount());
+
+  // Shutdown the disk manager and remove the temporary file we created.
+  disk_manager->ShutDown();
+}
+TEST(PageGuardTest_improve, ReadTset) {
+  const std::string db_name = "test.db";
+  const size_t buffer_pool_size = 5;
+  const size_t k = 2;
+
+  auto disk_manager = std::make_shared<DiskManagerUnlimitedMemory>();
+  auto bpm = std::make_shared<BufferPoolManager>(buffer_pool_size, disk_manager.get(), k);
+
+  page_id_t page_id_temp;
+  auto *page0 = bpm->NewPage(&page_id_temp);
+
+  // test ~ReadPageGuard()
+  {
+    auto reader_guard = bpm->FetchPageRead(page_id_temp);
+    EXPECT_EQ(2, page0->GetPinCount());
+  }
+  EXPECT_EQ(1, page0->GetPinCount());
+
+  // test ReadPageGuard(ReadPageGuard &&that)
+  {
+    auto reader_guard = bpm->FetchPageRead(page_id_temp);
+    auto reader_guard_2 = ReadPageGuard(std::move(reader_guard));
+    EXPECT_EQ(2, page0->GetPinCount());
+  }
+  EXPECT_EQ(1, page0->GetPinCount());
+
+  // test ReadPageGuard::operator=(ReadPageGuard &&that)
+  {
+    auto reader_guard_1 = bpm->FetchPageRead(page_id_temp);
+    auto reader_guard_2 = bpm->FetchPageRead(page_id_temp);
+    EXPECT_EQ(3, page0->GetPinCount());
+    reader_guard_1 = std::move(reader_guard_2);
+    EXPECT_EQ(2, page0->GetPinCount());
+  }
+  EXPECT_EQ(1, page0->GetPinCount());
 
   // Shutdown the disk manager and remove the temporary file we created.
   disk_manager->ShutDown();
